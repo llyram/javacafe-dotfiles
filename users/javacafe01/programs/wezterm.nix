3c30c5b6c05de6d5097c8cf44527384548d5505c
Gokul Swaminathan <gokulswamilive@gmail.com>
2022-05-16 16:57:14 -0700

    Modified Wezterm Config
    Made sure padding was not dependent on dpi

--- users/javacafe01/programs/wezterm.nix
+++ users/javacafe01/programs/wezterm.nix
@@ -138,7 +138,7 @@ with theme.colors;
       window_close_confirmation = "NeverPrompt",
 
       -- Padding
-      window_padding = {left = 45, right = 45, top = 45, bottom = 45},
+      window_padding = {left = 40pt, right = 40pt, top = 40pt, bottom = 40pt},
 
       -- No opacity
       inactive_pane_hsb = {saturation = 1.0, brightness = 1.0},
